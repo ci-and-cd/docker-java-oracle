2919a1d3aaceafcd9a5b8708557a3678967bff40
haolun zhang <opensource@infra.top>
2018-11-24 18:28:53 +0800

    add more default JAVA_OPTS in entrypoint.sh

--- docker/home/alpine/entrypoint.sh
+++ docker/home/alpine/entrypoint.sh
@@ -2,15 +2,28 @@
 
 set -e
 
+if [[ -z ${LOG_PATH+x} ]]; then export LOG_PATH="${HOME}/data/logs"; fi
 
+# Use CMS by default
+#JAVA_OPTS="${JAVA_OPTS} -XX:+UseConcMarkSweepGC -XX:CMSInitiatingOccupancyFraction=75"
+# Use G1 by default
+#JAVA_OPTS="${JAVA_OPTS} -XX:+UseG1GC"
+JAVA_OPTS="${JAVA_OPTS} -XX:-OmitStackTraceInFastThrow -XX:+PrintFlagsFinal";
+# http://stackoverflow.com/questions/137212/how-to-solve-performance-problem-with-java-securerandom
 JAVA_OPTS="${JAVA_OPTS} -Djava.security.egd=file:/dev/urandom";
-if [ -n "${SPRING_PROFILES_ACTIVE}" ]; then JAVA_OPTS="${JAVA_OPTS} -Dspring.profiles.active=${SPRING_PROFILES_ACTIVE}"; fi
+# Crash dump & Heap dump
+if [[ "${JVM_DUMP_DISABLED}" != "true" ]]; then JAVA_OPTS="${JAVA_OPTS} -XX:ErrorFile=${LOG_PATH}/hs_err_%p.log -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=${LOG_PATH}/"; fi
+# GC log
+if [[ "${JVM_GCLOG_DISABLED}" != "true" ]]; then JAVA_OPTS="${JAVA_OPTS} -Xloggc:${LOG_PATH}/gc_%p.log -XX:+PrintGCDateStamps -XX:+PrintGCDetails -XX:+PrintGCApplicationConcurrentTime -XX:+PrintGCApplicationStoppedTime -XX:+PrintGC"; fi
+if [[ -n "${SPRING_PROFILES_ACTIVE}" ]]; then JAVA_OPTS="${JAVA_OPTS} -Dspring.profiles.active=${SPRING_PROFILES_ACTIVE}"; fi
+export JAVA_TOOL_OPTIONS="${JAVA_OPTS}"
+
 . /opt/java_debug_monitor_profiler.sh
 
 # if command starts with an option, prepend java
-if [ "${1:0:1}" == '-' ]; then
+if [[ "${1:0:1}" == '-' ]]; then
     set -- java "$@" -jar *-exec.jar
-elif [ "${1:0:1}" != '/' ]; then
+elif [[ "${1:0:1}" != '/' ]]; then
     set -- java ${JAVA_OPTS} -jar *-exec.jar "$@"
 fi
 
